1786c205361c42a8790b183be149c67467fed772
Sanam Limbu <sudosanam@gmail.com>
2022-12-12 01:22:12 +0800

    add HTTP response model

---
+++ Models/HTTPResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace dotnet_crud_api.Models
+{
+	public class HTTPResponse
+	{
+		public string Code { get; set; } = string.Empty;
+		public string Message { get; set; } = string.Empty;
+		public object? ResponseData { get; set; }
+	}
+
+	public enum ResponseType
+	{
+		Success,
+		NotFound,
+		Failure,
+	}
+}
+
---
+++ Models/HTTPResponseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace dotnet_crud_api.Models
+{
+	public class HTTPResponseHandler
+	{
+		public static HTTPResponse GetExceptionResponse(Exception ex)
+		{
+			HTTPResponse resp = new HTTPResponse();
+			resp.Code = "500";
+			resp.Message = ex.Message;
+			return resp;
+		}
+		public static HTTPResponse GetHTTPResponse(ResponseType type, object? contract)
+		{
+			HTTPResponse resp = new HTTPResponse { ResponseData = contract };
+			switch (type)
+			{
+				case ResponseType.Success:
+					resp.Code = "200";
+					resp.Message = "Success";
+					break;
+				case ResponseType.NotFound:
+					resp.Code = "404";
+					resp.Message = "Not found";
+					break;
+			}
+			return resp;
+		}
+		
+	}
+}
+
